039ac5932ebc0a400cfcb433f5a6a6844ed46bf1
Shehan Maxwell <shehanbbn@gmail.com>
2023-10-23 21:27:35 +0530

    changed 1055 files

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://shehanmaxwell:jZZiv5zFm2zZ6rhO@nodeandexpressprojects.4popwk8.mongodb.net/Books-collection?retryWrites=true&w=majority';
