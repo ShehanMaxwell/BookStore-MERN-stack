45603bbaeda2152c6e588b118966cfa96d0b5866
Shehan Maxwell <shehanbbn@gmail.com>
2023-10-26 10:46:49 +0530

    changed 4 files

--- frontend/src/components/spinner.jsx
+++ frontend/src/components/spinner.jsx
@@ -1,7 +1,7 @@
-function spinner() {
+function Spinner() {
   return (
-    <div className='animate-png w-16 h-16 m-8 rounded-full bg-sky-600'></div>
+    <div className='animate-ping w-16 h-16 m-8 rounded-full bg-sky-600'></div>
   );
 }
 
-export default spinner;
+export default Spinner;
--- frontend/src/pages/CreateBook.jsx
+++
@@ -1,7 +0,0 @@
-// import React from 'react'
-
-export const CreateBook = () => {
-  return <div>CreateBooks</div>;
-};
-
-export default CreateBook;
