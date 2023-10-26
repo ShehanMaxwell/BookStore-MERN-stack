52be01b767208a8d3de397ce92c0bc650a46eff6
Shehan Maxwell <shehanbbn@gmail.com>
2023-10-26 16:05:18 +0530

    changed 5 files

--- frontend/src/components/home/BooksCard.jsx
+++ frontend/src/components/home/BooksCard.jsx
@@ -1,9 +1,23 @@
 // import React from 'react'
+// import { Link } from 'react-router-dom';
+// import { PiBookOpenTextLight } from 'react-icons/pi';
+// import { BiUserCircle } from 'react-icons/bi';
+// import { AiOutlineEdit } from 'react-icons/ai';
+// import { BsInfoCircle } from 'react-icons/bs';
+// import { MdOutlineDelete } from 'react-icons/md';
+import BooksSingleCard from '../BooksSingleCard';
+import PropTypes from 'prop-types';
 
-const BooksCard = () => {
+const BooksCard = ({ books }) => {
   return (
-    <div>BooksCard</div>
-  )
-}
-
-export default BooksCard
+    <div className='grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+      {books.map((item) => (
+        <BooksSingleCard key={item._id} book={item} />
+      ))}
+    </div>
+  );
+};
+BooksCard.propTypes = {
+  books: PropTypes.array.isRequired,
+};
+export default BooksCard;
